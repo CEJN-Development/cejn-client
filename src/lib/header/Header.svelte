9f261f9d9ab270f70cd2e45170d0988702c1d056
cejn_dev_user1 <diegoenriquezserrano@protonmail.com>
2021-11-27 20:12:32 -0600

    header

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -1,103 +1,126 @@
 <script lang="ts">
 	import { page } from '$app/stores';
-	import logo from './svelte-logo.svg';
+
+	let showNavbar:boolean = false;
 </script>
 
 <header>
-	<div class="corner">
-		<a href="https://kit.svelte.dev">
-			<img src={logo} alt="SvelteKit" />
-		</a>
-	</div>
-
 	<nav>
-		<svg viewBox="0 0 2 3" aria-hidden="true">
-			<path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
-		</svg>
-		<ul>
-			<li class:active={$page.path === '/'}><a sveltekit:prefetch href="/">Home</a></li>
-			<li class:active={$page.path === '/about'}><a sveltekit:prefetch href="/about">About</a></li>
-			<li class:active={$page.path === '/todos'}><a sveltekit:prefetch href="/todos">Todos</a></li>
-		</ul>
-		<svg viewBox="0 0 2 3" aria-hidden="true">
-			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
-		</svg>
+		<section id="logo" class="left">
+			<img src="/favicon.png" alt="Organization logo" />
+			<b class="container">
+				Chicago Environmental Justice Network
+			</b>
+		</section>
+		<section id="expander" class="right">
+			<button class="button" on:click={() => showNavbar = true}>
+				&#9776;
+			</button>
+		</section>
+		<section id="links" class:active={showNavbar}>
+			<section class="left">
+			</section>
+			<section class="right">
+				<button class="button flat" on:click={() => showNavbar = false}>
+					X
+				</button>
+			</section>
+			<ul>
+				<li class:active={$page.path === '/'}><a sveltekit:prefetch href="/">Home</a></li>
+				<li class:active={$page.path === '/about'}><a sveltekit:prefetch href="/about">About Us</a></li>
+				<li class:active={$page.path === '/our-stories'}><a sveltekit:prefetch href="/our-stories">Our Stories</a></li>
+				<li class:active={$page.path === '/in-the-news'}><a sveltekit:prefetch href="/in-the-news">CEJN in the News</a></li>
+				<li class:active={$page.path === '/who-are-we'}><a sveltekit:prefetch href="/who-are-we">Who is CEJN</a></li>
+				<li class:active={$page.path === '/press-releases'}><a sveltekit:prefetch href="/press-releases">Press</a></li>
+				<li class:active={$page.path === '/contact'}><a sveltekit:prefetch href="/contact">Contact</a></li>
+			</ul>
+		</section>
 	</nav>
-
-	<div class="corner">
-		<!-- TODO put something else here? github link? -->
-	</div>
 </header>
 
 <style>
 	header {
 		display: flex;
-		justify-content: space-between;
-	}
-
-	.corner {
-		width: 3em;
-		height: 3em;
-	}
-
-	.corner a {
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		width: 100%;
-		height: 100%;
-	}
-
-	.corner img {
-		width: 2em;
-		height: 2em;
-		object-fit: contain;
 	}
 
 	nav {
+		display: grid;
+		grid-template-areas: "logo expander";
+		grid-template-columns: 1fr 1fr;
+		width: 100%;
+		max-width: 100vw;
+		max-height: 4rem;
+	}
+
+	#links {
+		display: grid;
+		position: fixed;
+		top: 0;
+		left: 100%;
+		height: 100%;
+		width: 100%;
+		max-height: 100vh;
+		max-width: 100vw;
+		transition: left 0.3s ease-in-out;
+		background-color: rgba(255,255,255,1);
+		grid-template-areas:
+			"left right"
+			"links links";
+		grid-template-columns: 1fr 1fr;
+		grid-template-rows: 4rem 1fr;
+	}
+
+	#links.active {
+			left: 0%;
+		}
+
+	#links > .left {
+		grid-area: left;
+	}
+
+	#links > .right {
+		grid-area: right;
+	}
+
+	#logo {
+		grid-area: logo;
 		display: flex;
-		justify-content: center;
-		--background: rgba(255, 255, 255, 0.7);
+		max-height: 100%;
+		overflow: hidden;
 	}
 
-	svg {
-		width: 2em;
-		height: 3em;
-		display: block;
+	#logo b {
+		display: flex;
+		align-items: center;
 	}
 
-	path {
-		fill: var(--background);
+	#expander {
+		grid-area: expander;
+	}
+
+	.right {
+		display: flex;
+		align-items: center;
+		justify-content: end;
+		padding-right: 0.5rem;
 	}
 
 	ul {
 		position: relative;
 		padding: 0;
 		margin: 0;
-		height: 3em;
 		display: flex;
+		flex-direction: column;
 		justify-content: center;
 		align-items: center;
 		list-style: none;
-		background: var(--background);
 		background-size: contain;
+		grid-area: links;
 	}
 
 	li {
 		position: relative;
 		height: 100%;
-	}
-
-	li.active::before {
-		--size: 6px;
-		content: '';
-		width: 0;
-		height: 0;
-		position: absolute;
-		top: 0;
-		left: calc(50% - var(--size));
-		border: var(--size) solid transparent;
-		border-top: var(--size) solid var(--accent-color);
 	}
 
 	nav a {
@@ -106,15 +129,40 @@
 		align-items: center;
 		padding: 0 1em;
 		color: var(--heading-color);
-		font-weight: 700;
-		font-size: 0.8rem;
-		text-transform: uppercase;
-		letter-spacing: 0.1em;
 		text-decoration: none;
 		transition: color 0.2s linear;
 	}
 
 	a:hover {
 		color: var(--accent-color);
+	}
+
+	@media screen and (min-width: 500px) {
+		nav {
+			grid-template-areas:
+				"logo expander"
+				"links links";
+		}
+
+		#expander {
+			visibility: hidden;
+		}
+
+		#links {
+			position: relative;
+			display: flex;
+			left: 0;
+			grid-area: links;
+			justify-content: center;
+		}
+
+		#links .left,
+		#links .right {
+			display: none;
+		}
+
+		#links ul {
+			flex-direction: row;
+		}
 	}
 </style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,7 +3,6 @@
 </script>
 
 <script lang="ts">
-	import Counter from '$lib/Counter.svelte';
 </script>
 
 <svelte:head>
@@ -11,8 +10,6 @@
 </svelte:head>
 
 <section>
-
-	<Counter />
 </section>
 
 <style>
@@ -22,24 +19,5 @@
 		justify-content: center;
 		align-items: center;
 		flex: 1;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
 	}
 </style>
