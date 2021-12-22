0d285de2c4d5e2e97b4a709cc82d02c37de11d14
cejn_dev_user1 <diegoenriquezserrano@protonmail.com>
2021-12-22 06:45:35 -0600

    fetch bios from server

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,28 +1,25 @@
 <script context="module" lang="ts">
 	export const prerender = true;
-	
 	export async function load({ page, fetch, session, stuff }) {
 		const articlesRes = await fetch(`${import.meta.env.VITE_API_URL}/articles?limit=3`);
-		if (!articlesRes.ok) {
-			return {
-				status: articlesRes.status,
-				error: new Error("Could not load articles")
-			}
+		if (!articlesRes.ok) return {
+			status: articlesRes.status,
+			error: new Error("Could not load articles")
 		};
 
 		const biosRes = await fetch(`${import.meta.env.VITE_API_URL}/bios`);
-		if (!biosRes.ok) {
-			return {
-				status: biosRes.status,
-				error: new Error("Could not load bios")
-			}
+		if (!biosRes.ok) return {
+			status: biosRes.status,
+			error: new Error("Could not load bios")
 		};
 
 		let articles = await articlesRes.json();
+		let bios = await biosRes.json();
 
 		return {
 			props: {
-				articles
+				articles,
+				bios,
 			}
 		};
 	};
@@ -32,9 +29,11 @@
 	import LatestPosts from "$lib/shared/LatestPosts.svelte";
 	import WhoAreWe from "$lib/index/WhoAreWe.svelte";
 	import About from "$lib/index/About.svelte";
-	import type { Article } from "$lib/types/articles";
+	import type { Article } from "$lib/types/Articles";
+	import type { Bio } from "$lib/types/Bios";
 
 	export let articles:Article[];
+	export let bios:Bio[];
 </script>
 
 <svelte:head>
@@ -44,6 +43,6 @@
 <hr class="separator stack-48" />
 <About />
 <hr class="separator stack-48" />
-<WhoAreWe />
+<WhoAreWe {bios} />
 <hr class="separator stack-48" />
 <LatestPosts {articles} />
