b13dc47421775ee68b507039a820055c68370b4b
cejn_dev_user1 <cejndev@protonmail.com>
2022-02-19 15:50:51 -0600

    another fix for renaming tables

--- src/routes/admin/organizations/new.svelte
+++ src/routes/admin/organizations/new.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
-  import BioForm from '$lib/components/admin/Bios/BioForm.svelte';
+  import OrganizationForm from '$lib/components/admin/Organizations/OrganizationForm.svelte';
 </script>
 
 <h1 class="squish-16 squeeze-16">Our Contributors | New</h1>
 
-<BioForm />
+<OrganizationForm />
