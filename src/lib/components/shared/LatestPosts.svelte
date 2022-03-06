90be53bae27732c65ee22d54e175d865699c241b
cejn_dev_user1 <cejndev@protonmail.com>
2022-03-06 14:03:39 -0600

    add thumbnail to articles

--- src/lib/components/shared/LatestPosts.svelte
+++ src/lib/components/shared/LatestPosts.svelte
@@ -1,24 +1,35 @@
 <script lang="ts">
   import type { Article } from "$lib/types/Articles";
+  import CloudinaryImage from "$lib/components/shared/CloudinaryImage.svelte";
 
   export let articles:Article[] = [];
 </script>
 
-<h1 class="text-strong text-large stack-24">Latest Posts</h1>
+<h1 class="text-strong text-large stack-48">Recent Articles</h1>
 {#if articles?.length > 0}
   {#each articles as article}
-    <p class="text-strong text-normal stack-8">{article.title}</p>
-    <div class="text-medium text-normal stack-48">
-      {#each article.sample.split("\n\n") as paragraph}
-        <p class="stack-8">{@html paragraph}</p>
-      {/each}
-      <a href={`/articles/${article.slug}`}>Read full article &gt;&gt;</a>
-    </div>
-    <hr class="separator stack-24" />
+    <section class="flex-column gap-16 stack-48">
+      <div>
+        <CloudinaryImage
+            cloudinaryImageUrl={article.cloudinary_image_url}
+            options={{ height: 300, width: 350, crop: "fill" }}
+          />
+      </div>
+      <div>
+        <p class="text-strong text-normal stack-8">{article.title}</p>
+        <div class="text-medium text-normal">
+          {#each article.excerpt.split("\n\n") as paragraph}
+            <p class="stack-8">{@html paragraph}</p>
+          {/each}
+          <a href={`/articles/${article.slug}`}>Read full article &gt;&gt;</a>
+        </div>
+      </div>
+    </section>
+    <hr class="separator stack-48" />
   {/each}
 {:else}
-  <p class="text-medium text-normal stack-24">There are currently no published articles.</p>
-  <hr class="separator stack-24" />
+  <p class="text-medium text-normal stack-48">There are currently no published articles.</p>
+  <hr class="separator stack-48" />
 {/if}
 
 <style>
