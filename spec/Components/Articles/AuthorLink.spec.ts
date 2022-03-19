85e473942c21d6feee9518ac330881bf3bf42183
cejn_dev_user1 <cejndev@protonmail.com>
2022-03-19 14:22:21 -0500

    AuthorLink specs

--- spec/Components/Articles/AuthorLink.spec.ts
+++ spec/Components/Articles/AuthorLink.spec.ts
@@ -1,12 +1,24 @@
 import { render } from '@testing-library/svelte';
 import AuthorLink from '$lib/components/Articles/AuthorLink.svelte';
 
-it('Welcome the user', () => {
-  const props = { 
-    end: true,
-    author: { slug: 'author-name', full_name: 'Author Name'}
-   };
-	const { getByText } = render(AuthorLink, { props });
-  
-  expect(getByText('Author Name')).toBeInTheDocument();
+describe('AuthorLink', () => {
+	describe('with an author', () => {
+		const props = {
+			author: { slug: 'author-name', full_name: 'Author Name' },
+			end: true
+		};
+
+		it('renders the correct author name', () => {
+			const { getByText } = render(AuthorLink, { props });
+
+			expect(getByText('Author Name')).toBeInTheDocument();
+		});
+
+		it('links to the correct path', () => {
+			const { getByText } = render(AuthorLink, { props });
+			const authorLink = <HTMLAnchorElement>getByText('Author Name');
+
+			expect(authorLink.pathname).toEqual('/writers/author-name');
+		});
+	});
 });
