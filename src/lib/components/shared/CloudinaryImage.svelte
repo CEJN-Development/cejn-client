c3316c57ff479d6826516b6db5a6cfba0b565d05
CEJN-Development <94398499+CEJN-Development@users.noreply.github.com>
2022-03-19 17:42:40 -0500

    use environment variable for cloudinary base url (#31)

--- src/lib/components/shared/CloudinaryImage.svelte
+++ src/lib/components/shared/CloudinaryImage.svelte
@@ -1,53 +1,50 @@
 <script lang="ts">
-  import type { CloudinaryImageOptions } from "$lib/types/CloudinaryImage";
+	import type { CloudinaryImageOptions } from '$lib/types/CloudinaryImage';
 
-  export let cloudinaryImageUrl: string = "";
-  export let options: CloudinaryImageOptions = {};
-  export let classes: string = "";
+	export let cloudinaryImageUrl: string = '';
+	export let options: CloudinaryImageOptions = {};
+	export let classes: string = '';
 
-  const defaultOptions: CloudinaryImageOptions = {
-    width: 500,
-    height: 281,
-    crop: "fit",
-  };
+	const defaultOptions: CloudinaryImageOptions = {
+		width: 500,
+		height: 281,
+		crop: 'fit'
+	};
 
-  const cloudinaryBaseUrl = "https://res.cloudinary.com/cejn-dev/image/upload/";
+	const cloudinaryBaseUrl = <string>import.meta.env.VITE_CLOUDINARY_BASE_URL;
 
-  let cloudinaryPublicId: string;
-  let imagePath: string;
-  let cropping: string;
-  let dimensions: string;
-  let renderImage: boolean;
+	let cloudinaryPublicId: string;
+	let imagePath: string;
+	let cropping: string;
+	let dimensions: string;
+	let renderImage: boolean;
 
-  const getCropValue = (options: CloudinaryImageOptions) => {
-    if (options.crop == "fit") return "c_fit";
-    else if (options.crop == "fill") return "c_fill,g_auto";
-    else if (options.crop == "fill_pad") return "b_auto,c_fill_pad,g_auto";
-  };
+	const getCropValue = (options: CloudinaryImageOptions) => {
+		if (options.crop == 'fit') return 'c_fit';
+		else if (options.crop == 'fill') return 'c_fill,g_auto';
+		else if (options.crop == 'fill_pad') return 'b_auto,c_fill_pad,g_auto';
+	};
 
-  const getDimensions = (options: CloudinaryImageOptions) => {
-    return `h_${options.height},w_${options.width}`;
-  };
+	const getDimensions = (options: CloudinaryImageOptions) => {
+		return `h_${options.height},w_${options.width}`;
+	};
 
-  $: {
-    options = { ...defaultOptions, ...options };
-    cloudinaryPublicId = cloudinaryImageUrl?.split(cloudinaryBaseUrl).join("/");
-    renderImage = Number(cloudinaryImageUrl) != 0;// value could potentially be null or ""
-    cropping = getCropValue(options);
-    dimensions = getDimensions(options);
-    imagePath = `${cloudinaryBaseUrl}${cropping},${dimensions}${cloudinaryPublicId}`;
-  };
+	$: {
+		options = { ...defaultOptions, ...options };
+		cloudinaryPublicId = cloudinaryImageUrl?.split(cloudinaryBaseUrl).join('/');
+		renderImage = Number(cloudinaryImageUrl) != 0; // value could potentially be null or ""
+		cropping = getCropValue(options);
+		dimensions = getDimensions(options);
+		imagePath = `${cloudinaryBaseUrl}${cropping},${dimensions}${cloudinaryPublicId}`;
+	}
 </script>
 
 {#if renderImage}
-  <img
-    class={classes}
-    src={imagePath}
-  />
+	<img class={classes} src={imagePath} />
 {/if}
 
 <style>
-  img {
-    width: 100%;
-  }
+	img {
+		width: 100%;
+	}
 </style>
