bef8ae4cb9777e89d4df6536f882bcb41b1655ad
cejn_dev_user1 <cejndev@protonmail.com>
2022-06-08 08:20:07 -0500

    confirmation page

---
+++ src/lib/components/Confirmation/Success.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	export let email;
+</script>
+
+<p>Congratulations!</p>
+<p>You have successfully confirmed {email}!</p>
+<p class="stack-16">You can now set your password and start logging in</p>
+<button class="button button-primary text-strong flex-row">Set your password</button>
--- src/routes/confirmation/index.svelte
+++ src/routes/confirmation/index.svelte
@@ -2,19 +2,25 @@
 	export const prerender = false;
 
 	export async function load({ url, params, fetch, session, stuff }) {
-		const confirmation_token = url.searchParams.get('confirmation_token');
+		const confirmationToken = url.searchParams.get('confirmation_token');
 		const email = url.searchParams.get('email');
+
+		if (!email || !confirmationToken) {
+			return {
+				props: {
+					confirmation: { status: 201, message: { invalid: 'Confirmation Token is invalid.' } },
+					email
+				}
+			};
+		}
 
 		const confirmationRes = await fetch(
 			`${
 				import.meta.env.VITE_API_URL
-			}/confirmation?confirmation_token=${confirmation_token}&email=${email}`
+			}/confirmation?confirmation_token=${confirmationToken}&email=${email}`
 		);
 
-		let confirmation = {
-			status: confirmationRes.status,
-			message: await confirmationRes.json()
-		};
+		let confirmation = { status: confirmationRes.status, message: await confirmationRes.json() };
 
 		return {
 			props: {
@@ -25,10 +31,25 @@
 	}
 </script>
 
-<script>
-	export let confirmation;
-	export let email;
+<script lang="ts">
+	import ConfirmationExpired from '$lib/components/Confirmation/Errors.svelte';
+	import ConfirmationInvalid from '$lib/components/Confirmation/Invalid.svelte';
+	import ConfirmationSuccess from '$lib/components/Confirmation/Success.svelte';
 
-	console.log(email);
-	console.log(confirmation);
+	export let confirmation: { status: number; message: any };
+	export let email: string;
 </script>
+
+<main class="flex-row squeeze-24 squish-48 align-center">
+	<div class="max-width--mobile flex-row bordered border--rounded raised-2 squeeze-16 squish-16">
+		{#if confirmation.status === 401}
+			{#if confirmation.message.invalid}
+				<ConfirmationInvalid {confirmation} />
+			{:else if confirmation.message.email.includes('was already confirmed, please try signing in')}
+				<ConfirmationExpired {confirmation} />
+			{/if}
+		{:else}
+			<ConfirmationSuccess {email} />
+		{/if}
+	</div>
+</main>
