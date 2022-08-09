6df362b8ec8a87d8c76b1e4275ad60ac06817e5b
cejn_dev_user1 <cejndev@protonmail.com>
2022-08-09 16:15:08 -0500

    add new contact info

--- src/lib/components/shared/Contact.svelte
+++ src/lib/components/shared/Contact.svelte
@@ -3,8 +3,16 @@
 	<p>
 		Instagram: <a href="https://www.instagram.com/cejnetwork/?hl=en">@CEJNetwork</a>
 	</p>
-	<p class="stack-48">
+	<p>
 		Twitter: <a href="https://twitter.com/CEJNetwork/">@CEJNetwork</a>
+	</p>
+	<p>
+		Facebook: <a href="https://facebook.com/ChicagoEnvironmentalJusticeNetwork/">
+			@ChicagoEnvironmentalJusticeNetwork
+		</a>
+	</p>
+	<p class="stack-48">
+		Email: <a href="mailto:contact@chicagoejn.org"> contact@chicagoejn.org </a>
 	</p>
 	<p class="text-align-center">&copy; Chicago Environmental Justice Network 2022</p>
 </section>
